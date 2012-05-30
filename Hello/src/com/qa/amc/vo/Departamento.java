646278766d5ff84ac16f7db0d4afb49739ef1c07
josue.angulo28@gmail.com <josue.angulo28@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:38:08 +0000

    soy tu papi pinche beny

--- Hello/src/com/qa/amc/vo/Departamento.java
+++ Hello/src/com/qa/amc/vo/Departamento.java
@@ -1,5 +1,41 @@
 package com.qa.amc.vo;
-
+//@Author Joshua  tu papa ja ja ja
 public class Departamento {
+	
+	private Cliente  cliente ;
+	private String ubicacion ;
+	private String correo ;
+	private String nombreDelDepartamento ;
+	private Usuario responsable ;
+	public Cliente  getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente  cliente) {
+		this.cliente = cliente;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public String getNombreDelDepartamento() {
+		return nombreDelDepartamento;
+	}
+	public void setNombreDelDepartamento(String nombreDelDepartamento) {
+		this.nombreDelDepartamento = nombreDelDepartamento;
+	}
+	public Usuario getResponsable() {
+		return responsable;
+	}
+	public void setResponsable(Usuario responsable) {
+		this.responsable = responsable;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
 
 }
