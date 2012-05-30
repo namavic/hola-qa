170b1cdbe2ce4c3b01ea48b90ae82dc5f373ead9
jhovana.rodriguez.g@gmail.com <jhovana.rodriguez.g@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:52:03 +0000

    ...

--- trunk/Hello/src/com/qa/amc/vo/Proyecto.java
+++ trunk/Hello/src/com/qa/amc/vo/Proyecto.java
@@ -1,5 +1,74 @@
 package com.qa.amc.vo;
 
-public class Proyecto {
+import java.util.Date;
 
+public class Proyecto {
+	
+	private EquipoTrabajo equipoDeTrabajo;
+	private Cliente cliente;
+	private Departamento subDireccion;
+	private String status;
+	private Date fechaInicial;
+	private Date fechaFinal;
+	private String observaciones;
+
+	
+
+	public EquipoTrabajo getequipoDeTrabajo() {
+		return equipoDeTrabajo;
+	}
+
+	public void setEquipoDeTrabajo(EquipoTrabajo equipoDeTrabajo) {
+		this.equipoDeTrabajo = equipoDeTrabajo;
+	}
+
+	public Cliente getcliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Departamento getSubDireccion() {
+		return subDireccion;
+	}
+
+	public void setSubDireccion(Departamento subDireccion) {
+		this.subDireccion = subDireccion;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getFechaInicial() {
+		return fechaInicial;
+	}
+
+	public void setFechaInicial(Date fechaInicial) {
+		this.fechaInicial = fechaInicial;
+
+	}
+
+	public Date getFechaFinal() {
+		return fechaFinal;
+	}
+
+	public void setFechaFinal(Date fechaFinal) {
+		this.fechaFinal = fechaFinal;
+
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
 }
