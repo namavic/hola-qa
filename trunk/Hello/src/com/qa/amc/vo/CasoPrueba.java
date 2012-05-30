c88aa4159ff33954881913492a7276277f318933
edher.arizmendi.mtz@gmail.com <edher.arizmendi.mtz@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:08:32 +0000

    ya sabes

--- trunk/Hello/src/com/qa/amc/vo/CasoPrueba.java
+++ trunk/Hello/src/com/qa/amc/vo/CasoPrueba.java
@@ -1,5 +1,17 @@
 package com.qa.amc.vo;
 
 public class CasoPrueba {
+	private String ID;
+	private String Modulo;
+	private String Nombre;
+	private String Descripcion;
+	private String PasosParaResultado;
+	private String ResultadoEsperado;
+	private Date FechaEjecucion;
+	private String Estatus;
+	private String Precondiciones;
+	private String Poscondiciones;
+	private String Reejecuciones;
+	private String Comentarios;
 
 }
