b4c5c7415409e0b39817ee4006efdc982b75067c
pedrovalencia130@gmail.com <pedrovalencia130@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:52:22 +0000

    Se agrego los atributos a la clase PlanTrabajo

--- trunk/Hello/src/com/qa/amc/vo/PlanTrabajo.java
+++ trunk/Hello/src/com/qa/amc/vo/PlanTrabajo.java
@@ -1,5 +1,48 @@
 package com.qa.amc.vo;
 
+import java.util.Date;
+
 public class PlanTrabajo {
 
+	private String nombreProyecto;
+	private String duracion;
+	private Date fechaInicio;
+	private Date fechaFinal;
+	private Actividad[] listaActividades;
+	
+	
+	
+	public String getNombreProyecto() {
+		return nombreProyecto;
+	}
+
+	public void setNombreProyecto(String nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}
+	public String getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFinal() {
+		return fechaFinal;
+	}
+	public void setFechaFinal(Date fechaFinal) {
+		this.fechaFinal = fechaFinal;
+	}
+	public Actividad[] getListaActividades() {
+		return listaActividades;
+	}
+	public void setListaActividades(Actividad[] listaActividades) {
+		this.listaActividades = listaActividades;
+	}
+	
+	
 }
