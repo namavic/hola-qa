ac57037daa04b38895a4d22e90949e62d1769a51
edher.arizmendi.mtz@gmail.com <edher.arizmendi.mtz@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:33:29 +0000

    ya te la sabes

--- Hello/src/com/qa/amc/vo/CasoPrueba.java
+++ Hello/src/com/qa/amc/vo/CasoPrueba.java
@@ -1,7 +1,9 @@
 package com.qa.amc.vo;
 
+import java.util.Date;
+
 public class CasoPrueba {
-	private String ID;
+	
 	private String Modulo;
 	private String Nombre;
 	private String Descripcion;
@@ -14,4 +16,78 @@ public class CasoPrueba {
 	private String Reejecuciones;
 	private String Comentarios;
 
+	private String ID;
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getModulo() {
+		return Modulo;
+	}
+	public void setModulo(String modulo) {
+		Modulo = modulo;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public String getPasosParaResultado() {
+		return PasosParaResultado;
+	}
+	public void setPasosParaResultado(String pasosParaResultado) {
+		PasosParaResultado = pasosParaResultado;
+	}
+	public String getResultadoEsperado() {
+		return ResultadoEsperado;
+	}
+	public void setResultadoEsperado(String resultadoEsperado) {
+		ResultadoEsperado = resultadoEsperado;
+	}
+	public Date getFechaEjecucion() {
+		return FechaEjecucion;
+	}
+	public void setFechaEjecucion(Date fechaEjecucion) {
+		FechaEjecucion = fechaEjecucion;
+	}
+	public String getEstatus() {
+		return Estatus;
+	}
+	public void setEstatus(String estatus) {
+		Estatus = estatus;
+	}
+	public String getPrecondiciones() {
+		return Precondiciones;
+	}
+	public void setPrecondiciones(String precondiciones) {
+		Precondiciones = precondiciones;
+	}
+	public String getPoscondiciones() {
+		return Poscondiciones;
+	}
+	public void setPoscondiciones(String poscondiciones) {
+		Poscondiciones = poscondiciones;
+	}
+	public String getReejecuciones() {
+		return Reejecuciones;
+	}
+	public void setReejecuciones(String reejecuciones) {
+		Reejecuciones = reejecuciones;
+	}
+	public String getComentarios() {
+		return Comentarios;
+	}
+	public void setComentarios(String comentarios) {
+		Comentarios = comentarios;
+	}
 }
+	
