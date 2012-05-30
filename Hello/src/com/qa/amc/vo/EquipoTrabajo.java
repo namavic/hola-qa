d1f8bc2e4a19de5b7c61cd633007ecf6760e517f
pedrovalencia130@gmail.com <pedrovalencia130@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:51:54 +0000

    Se agrego los atributos a la clase EquipoTrabajo

--- Hello/src/com/qa/amc/vo/EquipoTrabajo.java
+++ Hello/src/com/qa/amc/vo/EquipoTrabajo.java
@@ -1,5 +1,60 @@
 package com.qa.amc.vo;
 
+import java.util.Date;
+
 public class EquipoTrabajo {
+	
+	private String nombre;
+	private String estatus;
+	private Date ultimaModificacion;
+	private Usuario liderProyecto;
+	private Usuario liderDesarrollo;
+	private String activo;
+	private String[] integrantes;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+	public Date getUltimaModificacion() {
+		return ultimaModificacion;
+	}
+	public void setUltimaModificacion(Date ultimaModificacion) {
+		this.ultimaModificacion = ultimaModificacion;
+	}
+	public Usuario getLiderProyecto() {
+		return liderProyecto;
+	}
+	public void setLiderProyecto(Usuario liderProyecto) {
+		this.liderProyecto = liderProyecto;
+	}
+	public Usuario getLiderDesarrollo() {
+		return liderDesarrollo;
+	}
+	public void setLiderDesarrollo(Usuario liderDesarrollo) {
+		this.liderDesarrollo = liderDesarrollo;
+	}
+	public String getActivo() {
+		return activo;
+	}
+	public void setActivo(String activo) {
+		this.activo = activo;
+	}
+	public String[] getIntegrantes() {
+		return integrantes;
+	}
+	public void setIntegrantes(String[] integrantes) {
+		this.integrantes = integrantes;
+	}
+	
 
 }
