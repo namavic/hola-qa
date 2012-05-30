6b53ccaf787c71f25d501d00efa9f652a741bfb4
edher.arizmendi.mtz@gmail.com <edher.arizmendi.mtz@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:08:56 +0000

    igual te la sabes

--- Hello/src/com/qa/amc/vo/Usuario.java
+++ Hello/src/com/qa/amc/vo/Usuario.java
@@ -1,5 +1,15 @@
 package com.qa.amc.vo;
 
 public class Usuario {
-
+	private String nombre;
+	private String ApellidoPaterno;
+	private String ApellidoMaterno;
+	private String Perfil;
+	private String Contraseña;
+	private String Correo;
+	private String Empresa;
+	private String CorreoAlterno;
+	private String Rol;
+	private Date FechaAlta;
+	private 	 Activo;
 }
