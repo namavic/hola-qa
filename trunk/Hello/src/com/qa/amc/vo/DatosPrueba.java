9f7fbab3d1132b26f06ad2c4c95687494ae6db24
josue.angulo28@gmail.com <josue.angulo28@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 17:43:33 +0000

    soy tu papi pinche beny

--- trunk/Hello/src/com/qa/amc/vo/DatosPrueba.java
+++ trunk/Hello/src/com/qa/amc/vo/DatosPrueba.java
@@ -1,5 +1,30 @@
+
 package com.qa.amc.vo;
 
 public class DatosPrueba {
+	private String [] dato ;
+	private CasoPrueba casoPrueba ;
+	
+
+
+	public CasoPrueba getCasosPrueba() {
+		return casoPrueba;
+	}
+
+	public void setCasosPrueba(CasoPrueba idcasosPrueba) {
+		this.casoPrueba = idcasosPrueba;
+	}
+
+	public String [] getDato() {
+		return dato;
+	}
+
+	public void setDato(String [] dato) {
+		this.dato = dato;
+	}
+	
+	
+	
+	
 
 }
