2d7db720dbeb521a44637fcee7a423a018d30137
edher.arizmendi.mtz@gmail.com <edher.arizmendi.mtz@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:34:00 +0000

    you are.....

--- Hello/src/com/qa/amc/vo/Usuario.java
+++ Hello/src/com/qa/amc/vo/Usuario.java
@@ -1,7 +1,8 @@
 package com.qa.amc.vo;
 
+import java.util.Date;
+
 public class Usuario {
-	private String nombre;
 	private String ApellidoPaterno;
 	private String ApellidoMaterno;
 	private String Perfil;
@@ -11,5 +12,73 @@ public class Usuario {
 	private String CorreoAlterno;
 	private String Rol;
 	private Date FechaAlta;
-	private 	 Activo;
+	
+
+	
+
+	
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidoPaterno() {
+		return ApellidoPaterno;
+	}
+	public void setApellidoPaterno(String apellidoPaterno) {
+		ApellidoPaterno = apellidoPaterno;
+	}
+	public String getApellidoMaterno() {
+		return ApellidoMaterno;
+	}
+	public void setApellidoMaterno(String apellidoMaterno) {
+		ApellidoMaterno = apellidoMaterno;
+	}
+	public String getPerfil() {
+		return Perfil;
+	}
+	public void setPerfil(String perfil) {
+		Perfil = perfil;
+	}
+	public String getContraseña() {
+		return Contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		Contraseña = contraseña;
+	}
+	public String getCorreo() {
+		return Correo;
+	}
+	public void setCorreo(String correo) {
+		Correo = correo;
+	}
+	public String getEmpresa() {
+		return Empresa;
+	}
+	public void setEmpresa(String empresa) {
+		Empresa = empresa;
+	}
+	public String getCorreoAlterno() {
+		return CorreoAlterno;
+	}
+	public void setCorreoAlterno(String correoAlterno) {
+		CorreoAlterno = correoAlterno;
+	}
+	public String getRol() {
+		return Rol;
+	}
+	public void setRol(String rol) {
+		Rol = rol;
+	}
+	public Date getFechaAlta() {
+		return FechaAlta;
+	}
+	public void setFechaAlta(Date fechaAlta) {
+		FechaAlta = fechaAlta;
+	}
 }
+	
+
+ 	
