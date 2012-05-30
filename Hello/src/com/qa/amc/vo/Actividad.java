b4d9659bf570d7d3a10f4f8c70c7334a68e3dff6
pedrovalencia130@gmail.com <pedrovalencia130@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-30 18:51:14 +0000

    Se agrego los atributos a la clase Actividad

--- Hello/src/com/qa/amc/vo/Actividad.java
+++ Hello/src/com/qa/amc/vo/Actividad.java
@@ -1,5 +1,65 @@
 package com.qa.amc.vo;
 
+import java.util.Date;
+
 public class Actividad {
+	private int numeroActividad;
+	private String actividad;
+	private String predecesora;
+	private Date fechaInicio;
+	private Date fechaTermino;
+	private int duracion;
+	private String recursoAsignado;
+	private String tipo;
+	
+	public int getNumeroActividad() {
+		return numeroActividad;
+	}
+		public void setNumeroActividad(int numeroActividad) {
+		this.numeroActividad = numeroActividad;
+	}
+	public String getActividad() {
+		return actividad;
+	}
+	public void setActividad(String actividad) {
+		this.actividad = actividad;
+	}
+	public String getPredecesora() {
+		return predecesora;
+	}
+	public void setPredecesora(String predecesora) {
+		this.predecesora = predecesora;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaTermino() {
+		return fechaTermino;
+	}
+	public void setFechaTermino(Date fechaTermino) {
+		this.fechaTermino = fechaTermino;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public String getRecursoAsignado() {
+		return recursoAsignado;
+	}
+	public void setRecursoAsignado(String recursoAsignado) {
+		this.recursoAsignado = recursoAsignado;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+		
 
 }
