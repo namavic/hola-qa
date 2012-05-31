08de15de82b590e4ff69a84b9cfa3f779f9fb9f9
beny.corona@gmail.com <beny.corona@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-31 15:27:53 +0000

    nuevos con h

--- trunk/Hello/src/com/qa/amc/vo/AnalisisAmbiguedades.java
+++ trunk/Hello/src/com/qa/amc/vo/AnalisisAmbiguedades.java
@@ -1,5 +1,57 @@
 package com.qa.amc.vo;
-
+import java.util.Date;
 public class AnalisisAmbiguedades {
-
+	/**
+	 * 
+	 */
+	private AnalisisAmbiguedades PlanTrabajo;
+	private Ambiguedad[] AnalisisAmbiguedades;
+	private PlanTrabajo[] nombreProyecto;
+	private Cliente[] propietarioProyecto;
+	private String defectoEncontrado;
+	private Date[] fechaComienzo;
+	private String comentario;
+	public AnalisisAmbiguedades getPlanTrabajo() {
+		return PlanTrabajo;
+	}
+	public void setPlanTrabajo(AnalisisAmbiguedades planTrabajo) {
+		PlanTrabajo = planTrabajo;
+	}
+	public Ambiguedad[] getAnalisisAmbiguedades() {
+		return AnalisisAmbiguedades;
+	}
+	public void setAnalisisAmbiguedades(Ambiguedad[] analisisAmbiguedades) {
+		AnalisisAmbiguedades = analisisAmbiguedades;
+	}
+	public PlanTrabajo[] getNombreProyecto() {
+		return nombreProyecto;
+	}
+	public void setNombreProyecto(PlanTrabajo[] nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}
+	public Cliente[] getPropietarioProyecto() {
+		return propietarioProyecto;
+	}
+	public void setPropietarioProyecto(Cliente[] propietarioProyecto) {
+		this.propietarioProyecto = propietarioProyecto;
+	}
+	public String getDefectoEncontrado() {
+		return defectoEncontrado;
+	}
+	public void setDefectoEncontrado(String defectoEncontrado) {
+		this.defectoEncontrado = defectoEncontrado;
+	}
+	public Date[] getFechaComienzo() {
+		return fechaComienzo;
+	}
+	public void setFechaComienzo(Date[] fechaComienzo) {
+		this.fechaComienzo = fechaComienzo;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
 }
