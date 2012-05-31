dc166ca2a71c146196fdb0dacf5245731987ca40
beny.corona@gmail.com <beny.corona@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-31 15:28:17 +0000

    nuevos con h2

--- trunk/Hello/src/com/qa/amc/vo/Roles.java
+++ trunk/Hello/src/com/qa/amc/vo/Roles.java
@@ -1,5 +1,33 @@
 package com.qa.amc.vo;
 
-public class Roles {
 
+public class Roles {
+		private Usuario rol;
+		private Pantalla perfil;
+		private String descripcion;
+		private String acciones;
+		public Usuario getRol() {
+			return rol;
+		}
+		public void setRol(Usuario rol) {
+			this.rol = rol;
+		}
+		public Pantalla getPerfil() {
+			return perfil;
+		}
+		public void setPerfil(Pantalla perfil) {
+			this.perfil = perfil;
+		}
+		public String getDescripcion() {
+			return descripcion;
+		}
+		public void setDescripcion(String descripcion) {
+			this.descripcion = descripcion;
+		}
+		public String getAcciones() {
+			return acciones;
+		}
+		public void setAcciones(String acciones) {
+			this.acciones = acciones;
+		}
 }
