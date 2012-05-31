3296b5190dbf08ab247b0cd4708304edacec399b
beny.corona@gmail.com <beny.corona@gmail.com@2033cbef-dd4b-8186-00df-f770d633faba>
2012-05-31 15:27:30 +0000

    qwert

--- trunk/Hello/src/com/qa/amc/vo/Ambiguedad.java
+++ trunk/Hello/src/com/qa/amc/vo/Ambiguedad.java
@@ -1,5 +1,19 @@
 package com.qa.amc.vo;
+import java.util.Date;
 
 public class Ambiguedad {
-
+	private AnalisisAmbiguedades[] Ambiguedad;
+	private Usuario[] usuario;
+	private String altaAmbiguedad;
+	private String paglinea;
+	private String referenciaProyecto;
+	private String sugerencia;
+	private String status;
+	private Usuario[] nombreTester;
+	private Date[] fechaIdentificacion;
+	private Date[] fechaUltimaModificacion;
+	private String solucionDefecto;
+	private Cliente personaAsignada;
+	private Date[] fechaCorreccion;
+	private String comentarios;
 }
